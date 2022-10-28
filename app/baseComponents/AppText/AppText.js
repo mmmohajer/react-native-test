e4d3598ee9c33b222e99da225e477ab8ae64e87e
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-10-28 12:17:54 -0400

    automations and shell scriptings added

---
+++ app/baseComponents/AppText/AppText.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+
+import { styles, fontStyleFunc } from "../../assets/styles";
+
+import { localStyles } from "./localStyles";
+
+const AppText = () => {
+  return (
+    <>
+      <View>
+        <Text>AppText</Text>
+      </View>
+    </>
+  );
+};
+
+export default AppText;
